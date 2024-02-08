62132a1e6c9f675e279b0f0eafdef310cebab553
Edwin Lim <edwin_lim@tech.gov.sg>
2024-02-08 20:11:51 +0800

    fix starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,40 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightTypeDoc, { typeDocSidebarGroup } from 'starlight-typedoc'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightTypeDoc, { typeDocSidebarGroup } from "starlight-typedoc";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Chainflow Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-						
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+  integrations: [
+    starlight({
+      title: "🌊hainflow",
+      social: {
+        github: "https://github.com/edwinlzs/chainflow",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          // items: [
+            // Each item here is one entry in the navigation menu.
+            // { label: "Get Started", link: "/guides/get-started/" },
+          // ],
+          autogenerate: { directory: "guides" },
+        },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
         typeDocSidebarGroup,
-			],
-			plugins: [
+      ],
+      plugins: [
         starlightTypeDoc({
-					// out: "./src/content/api/",
-						tsconfig: '../chainflow/tsconfig.json',
-						entryPoints: ["../chainflow/src/core", "../chainflow/src/http"],
-					typeDoc: {
-						entryPointStrategy: "expand",
-						exclude: ["**/*.test.ts", "**/logger.ts"],
-					},
+          tsconfig: "../chainflow/tsconfig.json",
+          entryPoints: ["../chainflow/src/core", "../chainflow/src/http"],
+          typeDoc: {
+            entryPointStrategy: "expand",
+            exclude: ["**/*.test.ts", "**/logger.ts"],
+          },
         }),
-			],
-		}),
-	],
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/guides/get-started.md
@@ -0,0 +1,52 @@
+---
+layout: /src/layouts/DocsLayout.astro
+title: Get Started
+description: A guide in my new Starlight docs site.
+---
+
+Chainflow is an API call workflow library written in and for TypeScript. Its aim is to improve the developer experience in managing multiple sets of API calls, where values of the response to one endpoint call can be passed to the request payload of another endpoint further down a workflow.
+
+## Installation
+
+```shell
+npm install --save-dev chainflow
+```
+
+## The Idea
+
+The concept behind Chainflow comes from "chaining" one API's outputs with another's inputs and running "flows" of API calls.
+
+Let's say a call to a `createUser` endpoint returns a new user's ID which can be passed to the request body of a call to the `addRole` endpoint to assign a role for that user. This represents a _"chain"_ between the two endpoints.
+
+We take advantage of that chain by writing a _"flow"_, where we explicitly instruct a `Chainflow` object to call `createUser` first, then `addRole` after. By ordering the flow in such a manner, we ensure that the user `id` response to `createUser` is dynamically used in the request body of `addRole`.
+
+```typescript
+chainflow().call(createUser).call(addRole).run();
+```
+
+## Basic Example
+
+Below, we create endpoints for an origin server, define the input nodes on their request body/query parameters, and run a flow calling those endpoints.
+
+```typescript
+import { originServer, chainflow } from "./chainflow";
+
+const origin = originServer("127.0.0.1:5000");
+
+// create endpoints
+const createUser = origin.post("/user").body({
+  name: "Tom",
+});
+
+const addRole = origin.post("/role").body({
+  userId: createUser.body.resp.id,
+  role: "Engineer",
+});
+
+const getUsersWithRole = origin.get("/user").query({
+  role: "Engineer",
+});
+
+// create flow
+chainflow().call(createUser).call(addRole).call(getUsersWithRole).run();
+```
